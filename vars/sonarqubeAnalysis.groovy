0070db13fde74865b574e5712243c96d2042c60d
uvais12khan <ukfriends33@gmail.com>
2024-06-27 15:34:19 +0530

    Update sonarqubeAnalysis.groovy

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,5 +1,5 @@
 def call() {
-    withSonarQubeEnv('sonar-server') {
+    withSonarQubeEnv('sonar-token') {
         sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Youtube -Dsonar.projectKey=Youtube '''
     }
 }
